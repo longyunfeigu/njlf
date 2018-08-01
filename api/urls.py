583c60c883d714803c631b0a610791f9a84dc391
longyunfeigu <2514553187@qq.com>
2018-08-01 17:31:15 +0800

    新闻api开发完成

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from .views.account import LoginView
 from .views.course import CourseView
+from .views.news import NewsView
 
 urlpatterns = [
     url('account/login/$', LoginView.as_view(), name='login'),
     url('course/$', CourseView.as_view({'get':'list'}), name='course'),
     url('course/(?P<pk>\d+)/$', CourseView.as_view({'get':'retrieve'}), name='course_detail'),
+    url('news/$', NewsView.as_view({'get':'list'}), name='news'),
+    url('news/(?P<pk>\d+)/$', NewsView.as_view({'get':'retrieve'}), name='news_detail'),
 ]
